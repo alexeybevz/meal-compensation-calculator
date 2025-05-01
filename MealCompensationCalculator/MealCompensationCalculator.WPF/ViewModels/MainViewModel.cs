963226b0c22f93b495b75be7c4c94884c02ac587
alexeybevz <bevz.alexei@yandex.ru>
2025-05-01 19:30:54 +0500

    add WPF project

---
+++ MealCompensationCalculator/MealCompensationCalculator.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MealCompensationCalculator.WPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MealCompensationCalculatorViewModel MealCompensationCalculatorViewModel { get; }
+
+        public MainViewModel()
+        {
+            MealCompensationCalculatorViewModel = MealCompensationCalculatorViewModel.LoadViewModel();
+        }
+    }
+}
---
+++ MealCompensationCalculator/MealCompensationCalculator.WPF/ViewModels/MealCompensationCalculatorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MealCompensationCalculator.WPF.ViewModels
+{
+    public class MealCompensationCalculatorViewModel : ViewModelBase
+    {
+        public static MealCompensationCalculatorViewModel LoadViewModel()
+        {
+            var vm = new MealCompensationCalculatorViewModel();
+            return vm;
+        }
+    }
+}
